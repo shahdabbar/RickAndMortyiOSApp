3eb755e76b89c319e0c1bb83773d522f19a78de8
shahdabbar <shahdabbar32@gmail.com>
2023-12-20 14:10:48 +0200

    Created Rick and Morty project

---
+++ RickAndMortyiOS/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RickAndMortyiOS
+//
+//  Created by Abbar on 20/12/2023.
+//
+
+import Foundation
+
+struct K {
+    static let characters = "Characters"
+    static let locations = "Locations"
+    static let episodes = "Episodes"
+    static let settings = "Settings"
+}
