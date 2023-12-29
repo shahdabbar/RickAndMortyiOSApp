c01e32a0659dfbc27dc8c4503460a38bb610e8e7
shahdabbar <shahdabbar32@gmail.com>
2023-12-29 17:51:20 +0200

    Create API Request

--- RickAndMortyiOS/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMortyiOS/Controllers/Core/RMCharacterViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 
 /// Controller to show and search for Characters
 final class RMCharacterViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBackground
         title = K.characters
+        
+        let request = RMRequest(
+            endpoint: .character,
+            pathComponents: ["1"],
+            queryParameters: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        
+        print(request.url!)
+        
+        RMService.shared.execute(request, expecting: String.self) { result in
+//            switch result {
+//            case .success()
+//            }
+        }
     }
 }
